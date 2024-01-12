f11afbbc37e7870068f9eda3400c581b27a3b512
Chavis Nguyen <chavis_nguyen@accton.com.vn>
2024-01-12 17:48:09 +0700

    update

--- src/me.ts
+++ src/me.ts
@@ -1,29 +1,90 @@
-
-import {groupBy} from "lodash";
+import _ from "lodash";
 
 import data from "./me.json" assert {type: "json"};
 
+console.log();
 
+Object.keys(data).filter(item => {
+  return item !== "undefined"
+}).forEach(key => {
+  // @ts-ignore
+  const grade = data[key]
+  const classes = _.groupBy(grade, x => x.Column6)
+  Object.keys(classes)
+    .filter(item => {
+      return item !== "undefined"
+    })
+    .forEach(item => {
+      console.log(`\n---\nLớp ${item}`)
+      genData(classes[item])
+    })
+})
 
+function percent(a: number, b: number) {
+  return `${((100 * a)/b).toFixed(2)}%`
+}
 
-const grade6 = data["6"]
+function getAvg(iClass: any[], key: string) {
+  let total = 0
 
+  iClass.forEach((stu) => {
+    if (!isNaN(stu[key])) {
+      total += Number(stu[key])
+    }
+  })
 
-const class6 = groupBy(grade6, x => x.Column6)
-// 6: Column7 = van | Column8 = toan | Column9 = anh
+  return (total/(iClass.length)).toFixed(2)
+}
 
-const class6A = class6["6A"]
-const length = class6A.length
+function genData(iClass: any[]) {
+  const length = iClass.length
+  console.log("Sĩ số lớp", length)
 
-console.log({length})
+  // for Văn:
 
+  console.log("Văn:")
 
-for (let i = 0; i <= 9; i++) {
-  const res = class6A
+  console.log("TB:", getAvg(iClass, "Column7"))
+
+  const fiveToTen = iClass
     .filter(item => item.Column7 !== undefined)
     .filter(item => {
       const value = item.Column7 as number
-      return value >= i && value < (i + 1)
+      return value >= 5 && value <= 10
     })
-  console.log(`${i} <= x < ${i + 1}`, res.length)
+  console.log(`Khoảng (5 <= x <= 10):`, fiveToTen.length, percent(fiveToTen.length, length))
+
+  for (let i = 0; i <= 9; i++) {
+    const res = iClass
+      .filter(item => item.Column7 !== undefined)
+      .filter(item => {
+        const value = item.Column7 as number
+        if (i == 9) {
+          return value >= 9 && value <= 10
+        }
+        return value >= i && value < (i + 1)
+      })
+    console.log(`${i} <= x < ${i + 1}`, res.length)
+  }
+
+  // for Anh:
+  console.log("Anh:")
+  console.log("TB:", getAvg(iClass, "Column9"))
+  const fiveToTen2 = iClass
+    .filter(item => item.Column9 !== undefined)
+    .filter(item => {
+      const value = item.Column9 as number
+      return value >= 5 && value <= 10
+    })
+  console.log(`5 <= x <= 10`, fiveToTen2.length, percent(fiveToTen2.length, length))
+
+  for (let i = 0; i <= 9; i++) {
+    const res = iClass
+      .filter(item => item.Column9 !== undefined)
+      .filter(item => {
+        const value = item.Column9 as number
+        return value >= i && value < (i + 1)
+      })
+    console.log(`${i} <= x < ${i + 1}`, res.length)
+  }
 }
